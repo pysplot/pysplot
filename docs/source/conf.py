5f7e95b754b11e7b1abdd17738372a5c5456677d
wenli.mo@jhuapl.edu <wenli.mo@jhuapl.edu>
2025-05-07 18:02:45 -0400

    fixing docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,4 +38,4 @@ source_suffix = {
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
